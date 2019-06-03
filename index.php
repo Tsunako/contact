09b40952fba7186acdee79a5c35ba9ec21b8274b
Tsunako <30615216+Tsunako@users.noreply.github.com>
2019-06-03 16:22:43 +0900

    Create index.php
    contactForm test

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+    $emailTo = "tsunako.nwdev@gmail.com";
+
+    $subject = "I hope this works!";
+
+    $body = "I think you're great!";
+
+    $headers = "From: rob@robpercival.co.uk";
+
+    if (mail($emailTo, $subject, $body, $headers)) {
+        
+        echo "The email was sent successfully";
+        
+    } else {
+        
+        echo "The email could not be sent.";
+        
+    }
+
+
+?>
